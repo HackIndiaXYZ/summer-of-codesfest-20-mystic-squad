059c49cd5f33fa64d84030939f23b1acab3e4cfe
John Varghese <126874846+John-Varghese-EH@users.noreply.github.com>
2026-07-30 21:01:33 +0530

    v1.0.0 alpha |J0X|

---
+++ frimware/EchoGazeFirmware/.pio/build/esp32dev/FrameworkArduino/USBCDC.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32dev/FrameworkArduino/USBCDC.cpp.o: \
+ /home/j0x/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBCDC.cpp \
+ /home/j0x/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.h \
+ /home/j0x/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
