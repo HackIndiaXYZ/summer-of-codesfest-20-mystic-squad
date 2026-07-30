cd0a63158c3e14c4b2b26b9d3c2eb51b100703c7
John Varghese <126874846+John-Varghese-EH@users.noreply.github.com>
2026-07-30 21:01:33 +0530

    v1.0.0 alpha |J0X|

---
+++ frimware/EchoGazeFirmware/.pio/build/esp32dev/lib05e/WebServer/detail/mimetable.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32dev/lib05e/WebServer/detail/mimetable.cpp.o: \
+ /home/j0x/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.cpp \
+ /home/j0x/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.h \
+ /home/j0x/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
