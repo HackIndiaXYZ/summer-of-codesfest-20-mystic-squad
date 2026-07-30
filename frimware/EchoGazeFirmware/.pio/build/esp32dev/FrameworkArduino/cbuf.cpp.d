23f6b5db6a3ce93e1e3765fa5a08877eb9046a28
John Varghese <126874846+John-Varghese-EH@users.noreply.github.com>
2026-07-31 00:46:26 +0530

    v1.0.9

--- frimware/EchoGazeFirmware/.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.o: \
- /home/j0x/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- /home/j0x/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
--- frimware/EchoGazeFirmware/.pio/build/esp32dev/lib14a/ESPAsyncWebServer/ChunkPrint.cpp.d
+++
@@ -1,7 +0,0 @@
-.pio/build/esp32dev/lib14a/ESPAsyncWebServer/ChunkPrint.cpp.o: \
- .pio/libdeps/esp32dev/ESPAsyncWebServer/src/ChunkPrint.cpp \
- .pio/libdeps/esp32dev/ESPAsyncWebServer/src/ChunkPrint.h \
- /home/j0x/.platformio/packages/framework-arduinoespressif32/cores/esp32/Print.h \
- /home/j0x/.platformio/packages/framework-arduinoespressif32/cores/esp32/WString.h \
- /home/j0x/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h \
- /home/j0x/.platformio/packages/framework-arduinoespressif32/cores/esp32/Printable.h
